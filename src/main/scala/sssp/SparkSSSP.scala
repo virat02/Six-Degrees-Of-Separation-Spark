4f4aac7db7336ebc426b9dcb855d433e7ceb9470
Javesh Monga <javeshmonga@gmail.com>
2019-03-23 14:16:05 -0400

    Distance RDD
    Created initial distance RDD

--- src/main/scala/sssp/SparkSSSP.scala
+++ src/main/scala/sssp/SparkSSSP.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 
 object SparkSSSP {
 
-    def main(args: Array[String]) {
+  def main(args: Array[String]) {
 
     val spark = SparkSession
       .builder
@@ -13,13 +13,21 @@ object SparkSSSP {
 
     val lines = spark.read.textFile(args(0)).rdd
 
-    val links = lines.map{ s =>
+    val graph = lines.map { s =>
       val parts = s.split(",")
       (parts(0), parts(1))
-    }.distinct().groupByKey().cache()
+    }
+      .distinct()
+      .groupByKey()
+      .cache()
+
+    val distances = graph
+      .map( x => if (x._1.toInt == 1) (x._1,0) else (x._1,-1))
 
 
-    links.saveAsTextFile(args(1))
 
+
+    distances.saveAsTextFile(args(1))
+
+        }
   }
-}
