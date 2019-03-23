a3df66acb036f6ea020f034fc34259a938268e46
Javesh Monga <javeshmonga@gmail.com>
2019-03-23 15:49:27 -0400

    Formed RDD containing all distances so far

--- src/main/scala/sssp/SparkSSSP.scala
+++ src/main/scala/sssp/SparkSSSP.scala
@@ -21,13 +21,27 @@ object SparkSSSP {
       .groupByKey()
       .cache()
 
-    val distances = graph
+    var distances = graph
       .map( x => if (x._1.toInt == 1) (x._1,0) else (x._1,-1))
 
+    var temp = distances
 
+    for (iterationCount<-1 to 10) {
 
+      temp = graph.join(temp)
+        .flatMap(x => x._2._1
+          .map(y => if (x._2._2 == -1){
+            (y, -1)
+          } else {
+            (y, x._2._2 + 1)
+          })
+        )
 
+      distances = temp.union(distances)
+
+      }
+
+    println(distances.collect().foreach(println))
     distances.saveAsTextFile(args(1))
-
-        }
   }
+}
