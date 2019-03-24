79bc79e3ab19392f5552b26eb55d252d8147fb9c
virat02 <virat02@ccs.neu.edu>
2019-03-24 16:05:34 -0400

    optimized code correctly set to remove unnecessary intermediate results

--- src/main/scala/sssp/App.scala
+++ src/main/scala/sssp/App.scala
@@ -4,20 +4,17 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.log4j.LogManager
 import org.apache.log4j.Level
+import util.control.Breaks._
 
 /**
   * @author ${user.name}
   */
 object App {
 
+  //"-1" denotes positive infinity
   def getMin(a: Int, b: Int): Int ={
-    if (a == -1){
-      return b
-    }
-
-    if (b == -1){
-      return a
-    }
+    if (a == -1) return b
+    if (b == -1) return a
 
     if (a < b) a else b
   }
@@ -36,37 +33,42 @@ object App {
       val parts = s.split(",")
       (parts(0), parts(1))
     }
+      //Filter out values above the threshold MAX
       .filter(x => x._1.toInt <= MAX && x._2.toInt <= MAX)
       .distinct()
       .groupByKey()
       .cache()
 
+    //Get the initial distance RDD
     var distances = graph
       .map( x => if (x._1.toInt == 1) (x._1,0) else (x._1,-1))
 
+    //distances will update after each iteration
     var temp = distances
 
     var done = false
-    while(!done) {
+    
+    while (!done) {
 
+      //distances, through different existing paths, for all the nodes reached so far
       temp = graph.join(temp)
-        .flatMap(x => x._2._1
-          .map(y => if (x._2._2 == -1){
-            (y, -1)
-          } else {
-            (y, x._2._2 + 1)
-          })
-        )
+          .filter(x => x._2._2 != -1)
+          .flatMap(x => x._2._1
+            .map(y => (y, x._2._2 + 1)))
 
-      val distances1 = temp.union(distances).reduceByKey((x,y) => getMin(x,y))
+      //updated distances for all nodes reached so far
+      val distances1 = temp.union(distances).reduceByKey((x, y) => getMin(x, y))
 
+      //check if any distance for any node has changed,
+      // as well as all nodes are visited from the given source to all it's targets
       done = distances.join(distances1)
-        .map{case (x,y) => y._1 == y._2}
-        .reduce((x,y) => x && y)
+        .map { case (x, y) => y._1 == y._2 }
+        .reduce((x, y) => x && y)
 
+      //Update the distances RDD only if it has changed from it's previous state
       distances = distances1
     }
-    println(distances.collect().foreach(println))
+
     distances.saveAsTextFile(args(1))
   }
 }
